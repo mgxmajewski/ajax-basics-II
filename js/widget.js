76cd0d6bc83d98e3b7accd81c7e9507f02833e09
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-30 23:00:57 +0100

    Adds initial AJAX request

---
+++ js/widget.js
@@ -0,0 +1,9 @@
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        console.log(xhr.responseText);
+    }
+};
+xhr.open ('GET', 'data/employees.json');
+xhr.send();
